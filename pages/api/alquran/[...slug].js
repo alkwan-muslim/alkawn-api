da3d62fad90b753dbab457488533964d5cc6b28c
unknown <jagadyudhaawali@gmail.com>
2021-10-01 19:38:47 +0700

    fix: slug exceed number of ayah

--- pages/api/alquran/[...slug].js
+++ pages/api/alquran/[...slug].js
@@ -2,33 +2,31 @@ export default async function handler(req, res) {
   const pid = req.query;
 
   const getAlquran = async () => {
-    return fetch(
-      `https://sahabat-alquran.jagad.xyz/alquran/${pid.slug[0]}.json`
-    );
+    return fetch(`https://alkawn-api.jagad.xyz/alquran/${pid.slug[0]}.json`);
   };
   const response = await getAlquran();
   const surah = await response.json();
+  const number = surah[pid.slug[0]].number;
+  const ayah_number = [pid.slug[1]];
+  const name = surah[pid.slug[0]].name;
+  const name_latin = surah[pid.slug[0]].name_latin;
+  const number_of_ayah = surah[pid.slug[0]].number_of_ayah;
+  const text = surah[pid.slug[0]].text[pid.slug[1]];
+  const translations = surah[pid.slug[0]].translations.id.text[pid.slug[1]];
+  const translations_name = surah[pid.slug[0]].translations.id.name;
+  const tafsir = surah[pid.slug[0]].tafsir.id.kemenag.text[pid.slug[1]];
+  const tafsir_name = surah[pid.slug[0]].tafsir.id.kemenag.name;
+  const tafsir_source = surah[pid.slug[0]].tafsir.id.kemenag.source;
 
   if (pid.slug.length === 1) {
     return res.status(200).json({ [pid.slug]: surah[pid.slug] });
   }
 
-  if (pid.slug.length === 2) {
-    const number = surah[pid.slug[0]].number;
-    const ayah_number = [pid.slug[1]];
-    const name = surah[pid.slug[0]].name;
-    const name_latin = surah[pid.slug[0]].name_latin;
-    const number_of_ayah = surah[pid.slug[0]].number_of_ayah;
-    const text = surah[pid.slug[0]].text[pid.slug[1]];
-    const translations = surah[pid.slug[0]].translations.id.text[pid.slug[1]];
-    const translations_name = surah[pid.slug[0]].translations.id.name;
-    const tafsir = surah[pid.slug[0]].tafsir.id.kemenag.text[pid.slug[1]];
-    const tafsir_name = surah[pid.slug[0]].tafsir.id.kemenag.name;
-    const tafsir_source = surah[pid.slug[0]].tafsir.id.kemenag.source;
+  if (parseInt(pid.slug[1]) > parseInt(number_of_ayah)) {
+    return res.status(200).json({ error: "number of ayah not found" });
+  }
 
-    if (pid.slug[1] > number_of_ayah) {
-      return res.status(200).json({ error: "number of ayah not found" });
-    }
+  if (pid.slug.length === 2) {
     return res.status(200).json({
       [number]: {
         number,
